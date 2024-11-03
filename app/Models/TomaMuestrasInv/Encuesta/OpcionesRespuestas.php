6a341ff59448c9f8863df98150153ea209eff1f7
JAPI <jose.barrios@mibcode.com>
2024-11-03 18:33:19 -0500

    renderizarEncuesta v1.5

--- app/Models/TomaMuestrasInv/Encuesta/OpcionesRespuestas.php
+++ app/Models/TomaMuestrasInv/Encuesta/OpcionesRespuestas.php
@@ -4,8 +4,17 @@ namespace App\Models\TomaMuestrasInv\Encuesta;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class OpcionesRespuestas extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'opciones_respuestas';
+
+    protected $fillable = [
+        'opcion',
+        'pregunta_id'
+    ];
 }
