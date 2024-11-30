ce9732b00b3d37e2a27122bcdaeb02e8c65e350f
JAPI <jose.barrios@mibcode.com>
2024-11-30 11:38:01 -0500

    asignacion_estante

---
+++ app/Models/TomaMuestrasInv/UbicacionBioBanco.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\TomaMuestrasInv;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UbicacionBioBanco extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/TomaMuestrasInv/UbicacionEstante.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\TomaMuestrasInv;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UbicacionEstante extends Model
+{
+    use HasFactory;
+}
